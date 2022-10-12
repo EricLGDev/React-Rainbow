44fc7c531f3ba0b4e5cf10e32cada7825ef818a5
Eric <ericlg.link@gmail.com>
2022-10-11 17:31:09 -0700

    Map colors to app page

--- src/App.js
+++ src/App.js
@@ -4,9 +4,15 @@ import './App.css';
 
 function App() {
   let colors = ['violet', 'blue', 'lightblue', 'green', 'greenyellow', 'yellow', 'orange', 'red']
+
+  let colorMap = colors.map((color, i) => {
+    return (
+      <ColorBlock color={color} />
+    )
+  })
   return (
     <div className="App">
-      
+      {colorMap}
     </div>
   );
 }
